c9a1307c41507bc6736e0e4e0a7084e068b990bc
Ivan <ivan.okhotnikov@outlook.com>
2022-05-23 15:33:33 +0100

    App formatting revised, efficiency displayed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from hst import HST
 
 
 def main():
+    st.set_page_config(page_title='HSU Performance', page_icon='fav.png')
     st.title('Modeled HSU Performance')
     st.header('Inputs')
     oil = st.selectbox('Oil', ('SAE 15W40', 'SAE 5W30', 'SAE 30'))
@@ -11,7 +12,7 @@ def main():
     hsu.compute_sizes()
     speed_pump = st.number_input('Pump speed, rpm:', 100, 5000, 2025)
     pressure_charge = st.number_input('Charge pressure, bar:', 10, 50, 25)
-    pressure_discharge = st.number_input('Discharge pressure, bar:', 35, 1000,
+    pressure_discharge = st.number_input('Discharge pressure, bar:', 40, 1000,
                                          472)
     hsu.compute_eff(speed_pump, pressure_discharge, pressure_charge)
     st.header('Pump')
@@ -24,6 +25,28 @@ def main():
     col1.metric('Speed, rpm', f"{hsu.performance['motor']['speed']:.2f}")
     col2.metric('Torque, Nm', f"{hsu.performance['motor']['torque']:.2f}")
     col3.metric('Power, kW', f"{hsu.performance['motor']['power']:.2f}")
+    with st.expander('Show efficiencies'):
+        st.header('Pump')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['pump']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['pump']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['pump']['total']:.2f}")
+        st.header('Motor')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['motor']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['motor']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['motor']['total']:.2f}")
+        st.header('HSU')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['hst']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['hst']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['hst']['total']:.2f}")
 
 
 if __name__ == "__main__":
