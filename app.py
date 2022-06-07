655590b1adffd9b592a09511a5e413ad8a6028f4
Ivan <ivan.okhotnikov@outlook.com>
2022-06-07 16:28:08 +0100

    Layout revised

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ st.set_page_config(page_title='HSU Performance', page_icon='fav.png')
 
 
 def main():
-    st.title('Modeled HSU Performance')
     with st.sidebar:
         st.header('Inputs')
         oil = st.selectbox('Oil', ('SAE 15W40', 'SAE 5W30', 'SAE 30'))
@@ -18,66 +17,77 @@ def main():
                                              1000, 472)
     hsu.compute_eff(speed_pump, pressure_discharge, pressure_charge)
     hsu.compute_control_flow()
-    st.header('HSU Performance')
-    st.subheader('Pump')
+    st.title('Modeled HSU Performance')
+    st.header('Pump')
     col1, col2, col3 = st.columns(3)
     col1.metric('Speed, rpm', f"{hsu.performance['pump']['speed']:.2f}")
     col2.metric('Torque, Nm', f"{hsu.performance['pump']['torque']:.2f}")
     col3.metric('Power, kW', f"{hsu.performance['pump']['power']:.2f}")
-    st.subheader('Motor')
+    st.header('Motor')
     col1, col2, col3 = st.columns(3)
     col1.metric('Speed, rpm', f"{hsu.performance['motor']['speed']:.2f}")
     col2.metric('Torque, Nm', f"{hsu.performance['motor']['torque']:.2f}")
     col3.metric('Power, kW', f"{hsu.performance['motor']['power']:.2f}")
-    st.header('Efficiencies')
-    st.subheader('Pump')
+    st.header('Charge pump')
     col1, col2, col3 = st.columns(3)
-    col1.metric('Volumetric, %',
-                f"{hsu.efficiencies['pump']['volumetric']:.2f}")
-    col2.metric('Mechanical, %',
-                f"{hsu.efficiencies['pump']['mechanical']:.2f}")
-    col3.metric('Total, %', f"{hsu.efficiencies['pump']['total']:.2f}")
-    st.subheader('Motor')
-    col1, col2, col3 = st.columns(3)
-    col1.metric('Volumetric, %',
-                f"{hsu.efficiencies['motor']['volumetric']:.2f}")
-    col2.metric('Mechanical, %',
-                f"{hsu.efficiencies['motor']['mechanical']:.2f}")
-    col3.metric('Total, %', f"{hsu.efficiencies['motor']['total']:.2f}")
-    st.subheader('HSU')
-    col1, col2, col3 = st.columns(3)
-    col1.metric('Volumetric, %',
-                f"{hsu.efficiencies['hst']['volumetric']:.2f}")
-    col2.metric('Mechanical, %',
-                f"{hsu.efficiencies['hst']['mechanical']:.2f}")
-    col3.metric('Total, %', f"{hsu.efficiencies['hst']['total']:.2f}")
-    st.header('Leakages')
-    st.subheader('Per pump/motor')
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric('Block, lpm', f"{hsu.performance['leakage']['block']*6e4:.2f}")
-    col2.metric('Shoes, lpm', f"{hsu.performance['leakage']['shoes']*6e4:.2f}")
-    col3.metric('Pistons, lpm',
-                f"{hsu.performance['leakage']['pistons']*6e4:.2f}")
-    col4.metric('Total, lpm', f"{hsu.performance['leakage']['total']*6e4:.2f}")
-    st.subheader('Per HSU')
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric('Blocks, lpm',
-                f"{hsu.performance['leakage']['block']*6e4*2:.2f}")
-    col2.metric('Shoes, lpm',
-                f"{hsu.performance['leakage']['shoes']*6e4*2:.2f}")
-    col3.metric('Pistons, lpm',
-                f"{hsu.performance['leakage']['pistons']*6e4*2:.2f}")
-    col4.metric('Total, lpm',
+    col1.metric('Continuous flow, lpm',
                 f"{hsu.performance['leakage']['total']*6e4*2:.2f}")
-    st.header('Charge flow')
-    col1, col2, col3 = st.columns(3)
-    col1.metric('Continuous, lpm',
-                f"{hsu.performance['leakage']['total']*6e4*2:.2f}")
-    col2.metric('Control, lpm', f"{hsu.control_flow*6e4*2:.2f}")
+    col2.metric('Control flow, lpm', f"{hsu.control_flow*6e4*2:.2f}")
     col3.metric(
-        'Transient, lpm',
+        'Transient flow, lpm',
         f"{(hsu.performance['leakage']['total']*2 + hsu.control_flow)*6e4:.2f}"
     )
+
+    with st.expander('Show detailed leakages'):
+        st.header('Leakages')
+        st.subheader('Cylinder block')
+        col1, col2 = st.columns(2)
+        col1.metric('Pump/motor, lpm',
+                    f"{hsu.performance['leakage']['block']*6e4:.2f}")
+        col2.metric('HSU, lpm',
+                    f"{hsu.performance['leakage']['block']*6e4*2:.2f}")
+        st.subheader('Shoes')
+        col1, col2 = st.columns(2)
+        col1.metric('Pump/motor, lpm',
+                    f"{hsu.performance['leakage']['shoes']*6e4:.2f}")
+        col2.metric('HSU, lpm',
+                    f"{hsu.performance['leakage']['shoes']*6e4*2:.2f}")
+        st.subheader('Pistons')
+        col1, col2 = st.columns(2)
+        col1.metric('Pump/motor, lpm',
+                    f"{hsu.performance['leakage']['pistons']*6e4:.2f}")
+        col2.metric('HSU, lpm',
+                    f"{hsu.performance['leakage']['pistons']*6e4*2:.2f}")
+        st.subheader('Total')
+        col1, col2 = st.columns(2)
+        col1.metric('Pump/motor, lpm',
+                    f"{hsu.performance['leakage']['total']*6e4:.2f}")
+        col2.metric('HSU, lpm',
+                    f"{hsu.performance['leakage']['total']*6e4*2:.2f}")
+
+    with st.expander('Show detailed efficiencies'):
+        st.header('Efficiencies')
+        st.subheader('Pump')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['pump']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['pump']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['pump']['total']:.2f}")
+        st.subheader('Motor')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['motor']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['motor']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['motor']['total']:.2f}")
+        st.subheader('HSU')
+        col1, col2, col3 = st.columns(3)
+        col1.metric('Volumetric, %',
+                    f"{hsu.efficiencies['hst']['volumetric']:.2f}")
+        col2.metric('Mechanical, %',
+                    f"{hsu.efficiencies['hst']['mechanical']:.2f}")
+        col3.metric('Total, %', f"{hsu.efficiencies['hst']['total']:.2f}")
 
 
 if __name__ == "__main__":
